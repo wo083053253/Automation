0a76ee825cf9ac549394dd13d57fc1e1f2745cef
Wo <walter.wo@sap.com>
2018-07-11 10:16:34 +0800

    update

--- deploy-rpm/roles/scripts/files/ndb_generate_rpms.sh
+++ deploy-rpm/roles/scripts/files/ndb_generate_rpms.sh
@@ -2,4 +2,4 @@
 hostID=$1
 rpmdir=$2
 time=$(date +"%Y%m%d_%H%M%S%p")
-echo "This is Mock to generate rpm package" > ${rpmdir}/ndb_${time}_${hostID}.rpm
+echo "This is Mock to generate rpm package" > ${rpmdir}/${time}_${hostID}.rpm
--- deploy-rpm/roles/scripts/files/rpo_generate_rpms.sh
+++ deploy-rpm/roles/scripts/files/rpo_generate_rpms.sh
@@ -2,4 +2,4 @@
 hostID=$1
 rpmdir=$2
 time=$(date +"%Y%m%d_%H%M%S%p")
-echo "This is Mock to generate rpm package" > ${rpmdir}/rpo_${hostID}_${time}.rpm
+echo "This is Mock to generate rpm package" > ${rpmdir}/${hostID}_${time}.rpm
